46ef6136879618abfa4cdfa6789e3cf5f7c79ae1
Jonas Kac <jonas@tuzame.com>
2014-07-13 23:46:43 -0300

    adding software-properties-common so that we can add outside PPAs

--- run.bash
+++ run.bash
@@ -141,7 +141,7 @@ function check_support {
 function install_basic_deps {
     echo "Updating apt-get and installing basic dependencies (this could take a while)..."
     sudo apt-get update
-    sudo apt-get install jq screen curl mercurial git bzr redis-server python-software-properties -qqy
+    sudo apt-get install jq screen curl mercurial git bzr redis-server software-properties-common python-software-properties -qqy
     sudo apt-add-repository ppa:tsuru/ppa -y >/dev/null 2>&1
     sudo apt-get update
 }
