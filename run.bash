4659955c53142c4ea2e36749389828a8ef3300e5
Francisco Souza <fss@corp.globo.com>
2015-07-02 09:58:27 -0300

    run: fallback to the old app-create call
    Fixing it for stable packages.

--- run.bash
+++ run.bash
@@ -480,7 +480,7 @@ function install_platform {
 
 function install_dashboard {
     echo "Installing tsuru-dashboard..."
-    tsuru app-create tsuru-dashboard python -o theonepool || true
+    tsuru app-create tsuru-dashboard python -o theonepool || tsuru app-create tsuru-dashboard python || true
     pushd ~/
     if [[ ! -e ~/tsuru-dashboard/app.yaml ]]; then
         git clone https://github.com/tsuru/tsuru-dashboard
