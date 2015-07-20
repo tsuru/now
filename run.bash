d3e6f11386f528e62e4f6f61d36eb77b8379b4d1
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-07-20 16:35:02 -0300

    Fix and refactor `public_ip`. Fix #27

--- run.bash
+++ run.bash
@@ -141,21 +141,43 @@ function installed_version {
     fi
 }
 
+function get_inet_addr() {
+    # Return IP address based on ifconfig's output
+    /sbin/ifconfig $1 2> /dev/null | grep "inet addr" | tail -n1 | \
+                                     sed "s/[^:]*:\([0-9.]*\).*/\1/"
+}
+
 function public_ip {
+    # Try to take the public IP using AWS EC2's metadata API:
     local ip=$(curl -s -L -m2 http://169.254.169.254/latest/meta-data/public-ipv4 || true)
-    if [[ $ip == "" ]]; then
-        ip=$(/sbin/ifconfig | grep -A1 eth | grep "inet addr" | tail -n1 | sed "s/[^0-9]*\([0-9.]*\).*/\1/")
+
+    # Try to use DigitalOcean's metadata API as fallback:
+    if [[ "$ip" == "" || "$ip" == "not found" ]]; then
+        ip=$(curl -s -L -m2 http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address || true)
     fi
-    if [[ $ip == "" ]]; then
-        ip=$(/sbin/ifconfig | grep -A1 venet0 | grep "inet addr" | tail -n1 | sed "s/[^0-9]*\([0-9.]*\).*/\1/")
+
+    # Try via ifconfig
+    if [[ "$ip" == "" || "$ip" == "not found" ]]; then
+        ip=$(get_inet_addr eth)
     fi
-    if [[ $ip == "" ]]; then
-        ip=$(ifconfig | grep -A1 wlan | grep "inet addr" | tail -n1 | sed "s/[^0-9]*\([0-9.]*\).*/\1/")
+    if [[ "$ip" == "" || "$ip" == "not found" ]]; then
+        ip=$(get_inet_addr venet0)
     fi
-    if [[ $ip == "" ]]; then
-        error "Couldn't find suitable public ip"
+    if [[ "$ip" == "" || "$ip" == "not found" ]]; then
+        ip=$(get_inet_addr wlan)
+    fi
+
+    # Try to access an external API to discover public IP
+    if [[ "$ip" == "" || "$ip" == "not found" ]]; then
+        ip=$(curl -s -L -m2 'https://api.ipify.org' || true)
+    fi
+
+    # Damn, it!
+    if [[ "$ip" == "" || "$ip" == "not found" ]]; then
+        error "Couldn't find a suitable public IP. Try using --host-ip option."
         exit 1
     fi
+
     echo "${ip}"
 }
 
