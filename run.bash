12a796265dee80eecce918b3053917ecc6447d7e
Andrews Medina <andrewsmedina@gmail.com>
2016-04-03 01:22:13 -0300

    removing GO15VENDOREXPERIMENT enviroment variable

--- run.bash
+++ run.bash
@@ -366,13 +366,11 @@ function install_go {
     if [[ ${GOPATH-} == "" ]]; then
         export GOPATH=$HOME/go
     fi
-    export GO15VENDOREXPERIMENT=1
     mkdir -p $GOPATH
     local bash_gopath=$(bash -ic 'unset GOPATH; source ~/.bashrc && echo $GOPATH')
     if [[ $bash_gopath != $GOPATH ]]; then
         echo "Adding GOPATH=$GOPATH to ~/.bashrc"
         echo -e "export GOPATH=$GOPATH" | tee -a ~/.bashrc > /dev/null
-        echo -e "export GO15VENDOREXPERIMENT=1" | tee -a ~/.bashrc > /dev/null
     fi
 }
 
